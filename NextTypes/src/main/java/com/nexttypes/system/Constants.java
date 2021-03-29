9849c94bded7aab481785eadeb8b4a6d5284cc9d
Alejandro Sánchez <alex@nexttypes.com>
2021-03-29 23:30:17 +0200

    Update to 1.3 version.

--- NextTypes/src/main/java/com/nexttypes/system/Constants.java
+++ NextTypes/src/main/java/com/nexttypes/system/Constants.java
@@ -18,7 +18,7 @@ package com.nexttypes.system;
 
 public class Constants {
 	public static final String NEXTTYPES = "NextTypes";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	
 	public static final String UTF_8_CHARSET = "UTF-8";
 	
